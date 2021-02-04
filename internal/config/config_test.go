8a50a5ff27678e769a8320e5d41a75990489a7a3
autopp <autopp.inc@gmail.com>
2021-02-04 21:39:44 +0900

    test: fix broken test

--- internal/config/config_test.go
+++ internal/config/config_test.go
@@ -4,19 +4,13 @@ import (
 	"io"
 	"os"
 	"path/filepath"
-	"runtime"
 	"testing"
 
 	"github.com/stretchr/testify/assert"
 )
 
 func openConfig(name string) io.Reader {
-	_, filename, _, ok := runtime.Caller(1)
-	if !ok {
-		panic("cannot load config " + name)
-	}
-
-	path := filepath.Join(filepath.Dir(filename), "..", "..", "example", name)
+	path := filepath.Join("testdata", name)
 	f, err := os.Open(path)
 	if err != nil {
 		panic(err)
@@ -29,10 +23,25 @@ func TestLoad(t *testing.T) {
 	r := openConfig("test.yaml")
 
 	if c, err := Load(r); assert.NoError(t, err) {
+		status := 0
+		stdout := "42\n"
 		expected := &Config{
 			Tests: []Test{
-				{Command: []string{"echo"}},
-				{Command: []string{"exit", "1"}},
+				{
+					Command: []string{"echo", "42"},
+					Env: []struct {
+						Name  string `yaml:"name"`
+						Value string `yaml:"value"`
+					}{{Name: "ANSWER", Value: "42"}},
+					Expect: &struct {
+						Status *int    `yaml:"status"`
+						Stdout *string `yaml:"stdout"`
+						Stderr *string `yaml:"stderr"`
+					}{
+						Status: &status,
+						Stdout: &stdout,
+					},
+				},
 			},
 		}
 
