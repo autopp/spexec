298e3c980d39591109a3b20ce530805aea633ad0
autopp <autopp.inc@gmail.com>
2021-02-20 10:57:49 +0900

    feat: add validator for map/seq

--- internal/config/validator.go
+++ internal/config/validator.go
@@ -5,6 +5,9 @@ import (
 	"strings"
 )
 
+type configMap map[string]interface{}
+type configSeq []interface{}
+
 type violation struct {
 	path    string
 	message string
@@ -47,6 +50,23 @@ func (v *validator) inIndex(index int, f func()) {
 	v.inPath(fmt.Sprintf("[%d]", index), f)
 }
 
-func (v *validator) addViolation(message string) {
+func (v *validator) addViolation(format string, args ...interface{}) {
+	message := fmt.Sprintf(format, args...)
 	v.violations = append(v.violations, violation{path: strings.Join(v.paths, ""), message: message})
+}
+
+func (v *validator) toBeMap(x interface{}) (configMap, bool) {
+	if m, ok := x.(configMap); ok {
+		return m, true
+	}
+	v.addViolation("should be map, but is %T", x)
+	return nil, false
+}
+
+func (v *validator) toBeSeq(x interface{}) (configSeq, bool) {
+	if s, ok := x.(configSeq); ok {
+		return s, true
+	}
+	v.addViolation("should be seq, but is %T", x)
+	return nil, false
 }
