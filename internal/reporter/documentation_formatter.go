714cd251d26027cfaf44873993cae11ce4e6ec08
autopp <autopp.inc@gmail.com>
2021-03-28 22:17:43 +0900

    feat: add documentation formatter

---
+++ internal/reporter/documentation_formatter.go
@@ -0,0 +1,64 @@
+// Copyright (C) 2021 Akira Tanimura (@autopp)
+//
+// Licensed under the Apache License, Version 2.0 (the “License”);
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+// http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an “AS IS” BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+package reporter
+
+import (
+	"fmt"
+
+	"github.com/autopp/spexec/internal/model"
+)
+
+/*
+DocumentationFormatter implements Reporter.
+
+Example of output:
+
+	.F.
+	3 examples, 1 failures
+*/
+type DocumentationFormatter struct{}
+
+// OnRunStart is part of Reporter
+func (sr *DocumentationFormatter) OnRunStart(w *Writer) {
+}
+
+// OnTestStart is part of Reporter
+func (sr *DocumentationFormatter) OnTestStart(w *Writer, t *model.Test) {
+}
+
+// OnTestComplete is part of Reporter
+func (sr *DocumentationFormatter) OnTestComplete(w *Writer, t *model.Test, tr *model.TestResult) {
+	var color Color
+	if tr.IsSuccess {
+		color = Green
+	} else {
+		color = Red
+	}
+
+	w.UseColor(color, func() {
+		fmt.Fprintln(w, t.Name)
+	})
+}
+
+// OnRunComplete is part of Reporter
+func (sr *DocumentationFormatter) OnRunComplete(w *Writer, trs []*model.TestResult) {
+	failures := 0
+	for _, tr := range trs {
+		if !tr.IsSuccess {
+			failures++
+		}
+	}
+	fmt.Fprintf(w, "\n%d examples, %d failures\n", len(trs), failures)
+}
