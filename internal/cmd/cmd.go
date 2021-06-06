18beeaee36ec1a38e0a784656a77783b4ced7489
autopp <autopp.inc@gmail.com>
2021-06-06 20:12:38 +0900

    feat: define builtin stream matchers

--- internal/cmd/cmd.go
+++ internal/cmd/cmd.go
@@ -20,8 +20,8 @@ import (
 	"os"
 
 	"github.com/autopp/spexec/internal/errors"
-	"github.com/autopp/spexec/internal/matcher"
 	"github.com/autopp/spexec/internal/matcher/status"
+	"github.com/autopp/spexec/internal/matcher/stream"
 	"github.com/autopp/spexec/internal/reporter"
 	"github.com/autopp/spexec/internal/runner"
 	"github.com/autopp/spexec/internal/spec/parser"
@@ -51,7 +51,7 @@ func Main(version string, stdin io.Reader, stdout, stderr io.Writer, args []stri
 
 			filename := args[0]
 			statusMR := status.NewStatusMatcherRegistryWithBuiltins()
-			streamMR := matcher.NewStreamMatcherRegistry()
+			streamMR := stream.NewStreamMatcherRegistryWithBuiltins()
 			tests, err := parser.New(statusMR, streamMR).ParseFile(filename)
 			if err != nil {
 				return err
