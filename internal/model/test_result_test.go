3057a3351827ece4aef38383436154537d1a64b5
autopp <autopp.inc@gmail.com>
2021-09-25 15:29:49 +0900

    test: add test for TestResult

---
+++ internal/model/test_result_test.go
@@ -0,0 +1,54 @@
+package model
+
+import (
+	. "github.com/onsi/ginkgo"
+	. "github.com/onsi/gomega"
+)
+
+var _ = Describe("SpecResult", func() {
+	Describe("NewSpecResult()", func() {
+		It("returns SpecResult with summary", func() {
+			trs := []*TestResult{
+				{
+					Name:      "test1",
+					IsSuccess: true,
+				},
+				{
+					Name:      "test2",
+					IsSuccess: false,
+				},
+				{
+					Name:      "test3",
+					IsSuccess: false,
+				},
+			}
+			sr := NewSpecResult(trs)
+			Expect(sr.Summary).To(Equal(SpecSummary{
+				NumberOfTests:     3,
+				NumberOfSucceeded: 1,
+				NumberOfFailed:    2,
+			}))
+		})
+	})
+
+	Describe("GetFailedTestResults()", func() {
+		It("returns failed test results only", func() {
+			trs := []*TestResult{
+				{
+					Name:      "test1",
+					IsSuccess: true,
+				},
+				{
+					Name:      "test2",
+					IsSuccess: false,
+				},
+				{
+					Name:      "test3",
+					IsSuccess: false,
+				},
+			}
+			sr := NewSpecResult(trs)
+			Expect(sr.GetFailedTestResults()).To(Equal([]*TestResult{trs[1], trs[2]}))
+		})
+	})
+})
