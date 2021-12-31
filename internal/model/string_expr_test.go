bfae9f3bcd51fd2cfedde28f50a86ddbfbf5ed50
autopp <autopp.inc@gmail.com>
2021-12-31 12:24:51 +0900

    test: test StringLiteralExpr

---
+++ internal/model/string_expr_test.go
@@ -0,0 +1,22 @@
+package model
+
+import (
+	. "github.com/onsi/ginkgo"
+	. "github.com/onsi/gomega"
+)
+
+var _ = Describe("StringLiteralExpr", func() {
+	literal := StringLiteralExpr("hello")
+
+	Describe("String()", func() {
+		It("returns itself", func() {
+			Expect(literal.String()).To(Equal("hello"))
+		})
+	})
+
+	Describe("Eval()", func() {
+		It("returns itself", func() {
+			Expect(literal.Eval()).To(Equal("hello"))
+		})
+	})
+})
