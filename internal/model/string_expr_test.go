6cff1b6504062ae9faa3ce521063e02c340f7d62
autopp <autopp.inc@gmail.com>
2022-02-10 23:04:15 +0900

    test: add test for EvalStringExprs

--- internal/model/string_expr_test.go
+++ internal/model/string_expr_test.go
@@ -1,7 +1,9 @@
 package model
 
 import (
+	"errors"
 	"os"
+	"strconv"
 
 	. "github.com/onsi/ginkgo/v2"
 	. "github.com/onsi/gomega"
@@ -64,4 +66,78 @@ var _ = Describe("envStringExpr", func() {
 			Expect(err).To(HaveOccurred())
 		})
 	})
+})
+
+type testStringExpr struct {
+	v              string
+	isEvaled       bool
+	successEval    bool
+	isCleanuped    bool
+	successCleanup bool
+}
+
+func (e *testStringExpr) Eval() (string, func() error, error) {
+	e.isEvaled = true
+	if !e.successEval {
+		return "", e.getCleanup(), errors.New(e.v)
+	}
+
+	return e.v, e.getCleanup(), nil
+}
+
+func (e *testStringExpr) getCleanup() func() error {
+	return func() error {
+		e.isCleanuped = true
+		if !e.successCleanup {
+			return errors.New(e.v)
+		}
+
+		return nil
+	}
+}
+
+func (e *testStringExpr) String() string {
+	return e.v
+}
+
+func extractBools(exprs []StringExpr, f func(e *testStringExpr) bool) []bool {
+	results := make([]bool, len(exprs))
+
+	for i, e := range exprs {
+		results[i] = f(e.(*testStringExpr))
+	}
+	return results
+}
+
+func (e *testStringExpr) stringExpr() {}
+
+var _ = Describe("EvalStringExprs()", func() {
+	DescribeTable("returns results, aggregated cleanup function, and errors",
+		func(successEvalAndCleanups [][2]bool, expectedValues []string, expectedEvaled []bool, expectedCleanuped []bool, expectedErr string, expectedCleanupErrs []string) {
+			exprs := make([]StringExpr, len(successEvalAndCleanups))
+			for i, fields := range successEvalAndCleanups {
+				expr := &testStringExpr{v: strconv.Itoa(i), successEval: fields[0], successCleanup: fields[1]}
+				exprs[i] = expr
+			}
+
+			values, cleanup, err := EvalStringExprs(exprs)
+
+			if expectedErr == "" {
+				Expect(err).NotTo(HaveOccurred())
+				Expect(values).To(Equal(expectedValues))
+			} else {
+				Expect(err).To(MatchError(expectedErr))
+				Expect(values).To(BeNil())
+			}
+
+			Expect(extractBools(exprs, func(e *testStringExpr) bool { return e.isEvaled })).To(Equal(expectedEvaled))
+			cleanupErrs := make([]string, 0)
+			for _, err := range cleanup() {
+				cleanupErrs = append(cleanupErrs, err.Error())
+			}
+			Expect(cleanupErrs).To(Equal(expectedCleanupErrs))
+			Expect(extractBools(exprs, func(e *testStringExpr) bool { return e.isCleanuped })).To(Equal(expectedCleanuped))
+		},
+		Entry("with 3 exprs all success", [][2]bool{{true, true}, {true, true}, {true, true}}, []string{"0", "1", "2"}, []bool{true, true, true}, []bool{true, true, true}, "", []string{}),
+	)
 })
