35348aed71328f8365461598d070fb23b0384054
autopp <autopp.inc@gmail.com>
2022-03-06 12:01:43 +0900

    test: add test case about of timeout

--- internal/exec/exec_test.go
+++ internal/exec/exec_test.go
@@ -1,6 +1,8 @@
 package exec
 
 import (
+	"time"
+
 	. "github.com/onsi/ginkgo/v2"
 	. "github.com/onsi/gomega"
 )
@@ -8,7 +10,7 @@ import (
 var _ = Describe("Exec", func() {
 	DescribeTable("Run()",
 		// TODO: test about of signal
-		func(e *Exec, exited bool, status int, signal string, stdout, stderr string) {
+		func(e *Exec, exited bool, status int, signal string, stdout, stderr string, isTimeout bool) {
 			if e.Timeout == 0 {
 				e.Timeout = defaultTimeout
 			}
@@ -20,33 +22,43 @@ var _ = Describe("Exec", func() {
 			if exited {
 				Expect(er.Status).To(Equal(status))
 			} else {
-				Expect(er.Signal.String()).To(Equal(signal))
+				if !isTimeout {
+					Expect(er.Signal.String()).To(Equal(signal))
+				}
 			}
+			Expect(er.IsTimeout).To(Equal(isTimeout))
 		},
 		Entry("with `echo -n 42`",
 			&Exec{
 				Command: []string{"echo", "-n", "42"},
 			},
-			true, 0, "", "42", "",
+			true, 0, "", "42", "", false,
 		),
 		Entry("with `echo -n 42 >&2`",
 			&Exec{
 				Command: []string{"testdata/stderr.sh"},
 			},
-			true, 0, "", "", "42",
+			true, 0, "", "", "42", false,
 		),
 		Entry("with `echo -n 42 >&2 (in ./testdata)`",
 			&Exec{
 				Command: []string{"./stderr.sh"},
 				Dir:     "testdata",
 			},
-			true, 0, "", "", "42",
+			true, 0, "", "", "42", false,
 		),
 		Entry("with `kill -TERM $$`",
 			&Exec{
 				Command: []string{"testdata/signal.sh"},
 			},
-			false, 0, "terminated", "", "",
+			false, 0, "terminated", "", "", false,
+		),
+		Entry("with `sleep 1` and 1ms timeout",
+			&Exec{
+				Command: []string{"sleep", "1"},
+				Timeout: 1 * time.Millisecond,
+			},
+			false, 0, "", "", "", true,
 		),
 	)
 })
