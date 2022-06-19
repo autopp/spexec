a5324b39a82422b07c92df2e3ab3b6b79b2a848b
autopp <autopp.inc@gmail.com>
2022-06-19 23:04:40 +0900

    feat: impl TestTemplate.Expand

--- internal/template/test_template.go
+++ internal/template/test_template.go
@@ -17,7 +17,10 @@ package template
 import (
 	"time"
 
+	"github.com/autopp/spexec/internal/matcher"
 	"github.com/autopp/spexec/internal/model"
+	"github.com/autopp/spexec/internal/spec"
+	"github.com/autopp/spexec/internal/util"
 )
 
 type TemplatableStringVar struct {
@@ -36,6 +39,76 @@ type TestTemplate struct {
 	StderrMatcher model.Templatable[any]
 	Env           []TemplatableStringVar
 	Timeout       time.Duration
-	TeeStdout     model.Templatable[bool]
-	TeeStderr     model.Templatable[bool]
+	TeeStdout     bool
+	TeeStderr     bool
+}
+
+func (tt *TestTemplate) Expand(env *model.Env, v *spec.Validator, statusMR matcher.StatusMatcherRegistry, streamMR matcher.StreamMatcherRegistry) (*model.Test, error) {
+	name, err := tt.Name.Expand(env)
+	if err != nil {
+		return nil, err
+	}
+
+	dir, err := tt.Dir.Expand(env)
+	if err != nil {
+		return nil, err
+	}
+
+	command := make([]model.StringExpr, 0, len(tt.Command))
+	for _, ct := range tt.Command {
+		c, err := ct.Expand(env)
+		if err != nil {
+			return nil, err
+		}
+
+		command = append(command, c)
+	}
+
+	stdin, err := tt.Stdin.Expand(env)
+	if err != nil {
+		return nil, err
+	}
+
+	status, err := tt.StatusMatcher.Expand(env)
+	if err != nil {
+		return nil, err
+	}
+	statusMatcher := statusMR.ParseMatcher(env, v, status)
+
+	stdout, err := tt.StdoutMatcher.Expand(env)
+	if err != nil {
+		return nil, err
+	}
+	stdoutMatcher := streamMR.ParseMatcher(env, v, stdout)
+
+	stderr, err := tt.StderrMatcher.Expand(env)
+	if err != nil {
+		return nil, err
+	}
+	stderrMatcher := streamMR.ParseMatcher(env, v, stderr)
+
+	tEnv := make([]util.StringVar, 0, len(tt.Env))
+	for _, tsv := range tt.Env {
+		value, err := tsv.Value.Expand(env)
+		if err != nil {
+			return nil, err
+		}
+
+		tEnv = append(tEnv, util.StringVar{Name: tsv.Name, Value: value})
+	}
+
+	return &model.Test{
+		Name:          name,
+		SpecFilename:  tt.SpecFilename,
+		Dir:           dir,
+		Command:       command,
+		Stdin:         []byte(stdin),
+		StatusMatcher: statusMatcher,
+		StdoutMatcher: stdoutMatcher,
+		StderrMatcher: stderrMatcher,
+		Env:           tEnv,
+		Timeout:       tt.Timeout,
+		TeeStdout:     tt.TeeStdout,
+		TeeStderr:     tt.TeeStderr,
+	}, nil
 }
