9842c71f92eb9abdb56eff6fbdf20eaba6b9d28d
autopp <autopp.inc@gmail.com>
2022-08-07 11:29:20 +0900

    test: add using placeholder case

--- internal/model/template/test_template_test.go
+++ internal/model/template/test_template_test.go
@@ -15,7 +15,15 @@ var _ = Describe("TestTemplate", func() {
 	Describe("Expand()", func() {
 		DescribeTable("success cases",
 			func(tt *TestTemplate, expected *model.Test) {
+				// Arrenge
 				env := model.NewEnv(nil)
+				env.Define("name", "sample test")
+				env.Define("dir", "")
+				env.Define("command", "echo")
+				env.Define("envMessage", "hello")
+				env.Define("statusMatcher", model.Map{"statusExample": nil})
+				env.Define("streamMatcher", model.Map{"streamExample": nil})
+
 				v, _ := model.NewValidator("")
 				statusMR := matcher.NewStatusMatcherRegistry()
 				statusMatcherParser, _ := testutil.GenParseExampleStatusMatcher(true, "message", nil)
@@ -25,6 +33,7 @@ var _ = Describe("TestTemplate", func() {
 				streamMatcherParser, _ := testutil.GenParseExampleStreamMatcher(true, "message", nil)
 				streamMR.Add("streamExample", streamMatcherParser)
 
+				// Act & Assert
 				Expect(tt.Expand(env, v, statusMR, streamMR)).To(Equal(expected))
 			},
 			Entry("with no placeholder",
@@ -45,6 +54,43 @@ var _ = Describe("TestTemplate", func() {
 					StatusMatcher: model.NewTemplatableFromValue[any](model.Map{"statusExample": nil}),
 					StdoutMatcher: model.NewTemplatableFromValue[any](model.Map{"streamExample": nil}),
 					StderrMatcher: model.NewTemplatableFromValue[any](model.Map{"streamExample": nil}),
+					TeeStdout:     false,
+					TeeStderr:     false,
+					Timeout:       1 * time.Second,
+				},
+				&model.Test{
+					Name:          "sample test",
+					SpecFilename:  "sample.yaml",
+					Dir:           "",
+					Command:       []model.StringExpr{model.NewLiteralStringExpr("echo")},
+					Stdin:         []byte(""),
+					Env:           []util.StringVar{{Name: "MESSAGE", Value: "hello"}},
+					StatusMatcher: testutil.NewExampleStatusMatcher(true, "message", nil),
+					StdoutMatcher: testutil.NewExampleStreamMatcher(true, "message", nil),
+					StderrMatcher: testutil.NewExampleStreamMatcher(true, "message", nil),
+					TeeStdout:     false,
+					TeeStderr:     false,
+					Timeout:       1 * time.Second,
+				},
+			),
+			Entry("with placeholders",
+				&TestTemplate{
+					Name:         model.NewTemplatableFromTemplateValue[string](model.NewTemplateValue(model.Map{"$": "name"}, []model.TemplateRef{model.NewTemplateVar("name")})),
+					SpecFilename: "sample.yaml",
+					Dir:          model.NewTemplatableFromValue(""),
+					Command: []*model.Templatable[model.StringExpr]{
+						model.NewTemplatableFromValue(model.NewLiteralStringExpr("echo")),
+					},
+					Stdin: model.NewTemplatableFromValue(""),
+					Env: []TemplatableStringVar{
+						{
+							Name:  "MESSAGE",
+							Value: model.NewTemplatableFromValue("hello"),
+						},
+					},
+					StatusMatcher: model.NewTemplatableFromTemplateValue[any](model.NewTemplateValue(model.Map{"$": "statusMatcher"}, []model.TemplateRef{model.NewTemplateVar("statusMatcher")})),
+					StdoutMatcher: model.NewTemplatableFromTemplateValue[any](model.NewTemplateValue(model.Map{"$": "streamMatcher"}, []model.TemplateRef{model.NewTemplateVar("streamMatcher")})),
+					StderrMatcher: model.NewTemplatableFromTemplateValue[any](model.NewTemplateValue(model.Map{"$": "streamMatcher"}, []model.TemplateRef{model.NewTemplateVar("streamMatcher")})),
 					TeeStdout:     false,
 					TeeStderr:     false,
 					Timeout:       1 * time.Second,
